294279f09631841239e1462cf8d81154ec1f05eb
Elif Karakutuk Dinc <elifkaraktk@gmail.com>
2020-04-01 11:10:40 +0200

    beverage analysis 2

--- top_10_price_of_beverages.sql
+++ top_10_price_of_beverages.sql
@@ -1,6 +1,6 @@
 --top 10 price of beverages 
 
-select item_description, max(state_bottle_cost),max(state_bottle_retail)
+select item_description, max(state_bottle_cost) as max_bottle_cost ,max(state_bottle_retail) as  max_bottle_cost_retail
 from sales_main_data.year_2019 
 group by 1
 order by 2 desc
